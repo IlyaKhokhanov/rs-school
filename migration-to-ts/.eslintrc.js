8db7ec039a3e2eff2c026eb3bc457049612dfe0c
IlyaKhokhanov <hohanov.ilya@gmail.com>
2023-06-04 22:24:29 +0300

    feat: add TS and eslint

---
+++ migration-to-ts/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+  },
+  ignorePatterns: ['*config.js', '*config.js', '.eslintrc.js'],
+};
