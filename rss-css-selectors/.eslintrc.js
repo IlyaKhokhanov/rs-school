0e9c7c8f90a431a86155eb2f3c52488dfb294406
IlyaKhokhanov <hohanov.ilya@gmail.com>
2023-06-11 13:37:34 +0300

    feat: make start config

---
+++ rss-css-selectors/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+  },
+  ignorePatterns: ['*config.js', '*config.js', '.eslintrc.js'],
+};
